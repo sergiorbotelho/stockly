fc3666893969a73ead488fb9e9aa7d23e33e8d1f
Sergio Botelho <sergiorbotelho@gmail.com>
2026-03-07 23:02:06 -0300

    feat: ensuree product stock is restored aftere sale is deleted

--- app/_actions/sales/delete-sale/index.ts
+++ app/_actions/sales/delete-sale/index.ts
@@ -7,10 +7,37 @@ import { deleteSaleSchema } from "./schema";
 export const deleteSale = actionClient
   .schema(deleteSaleSchema)
   .action(async ({ parsedInput: { id } }) => {
-    await db.sale.delete({
-      where: {
-        id,
-      },
+    await db.$transaction(async (trx) => {
+      const sale = await trx.sale.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          products: true,
+        },
+      });
+      if (!sale) return;
+      await trx.sale.delete({
+        where: {
+          id,
+        },
+      });
+
+      for (const product of sale?.products) {
+        await trx.product.update({
+          where: {
+            id: product.productId,
+          },
+          data: {
+            stock: {
+              increment: product.quantity,
+            },
+          },
+        });
+      }
     });
+
     revalidatePath("/sales");
+    revalidatePath("/products");
+    revalidatePath("/");
   });
